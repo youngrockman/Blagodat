bd6976ed60e6ba6df8abacb27b0731ef8cdbd4cb
Ivan <you@example.com>
2025-03-05 12:25:24 +0300

    init commit

--- Libary/obj/Debug/net9.0/Libary.AssemblyInfo.cs
+++ Libary/obj/Debug/net9.0/Libary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Libary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183967186553f72e72d2adb5b3b453e4514ff042")]
 [assembly: System.Reflection.AssemblyProductAttribute("Libary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Libary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
